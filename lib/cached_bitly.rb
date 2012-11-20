1856f33546292e17691ca2da10a5c3b58aad8add
dewski <me@garrettbjerkhoel.com>
2012-11-20 15:34:51 -0800

    Move CachedBitly to a module

--- lib/cached_bitly.rb
+++ lib/cached_bitly.rb
@@ -2,115 +2,129 @@ require 'cached_bitly/version'
 require 'redis'
 require 'bitly'
 
-class ShortenUrlService
-  class << self
-    def redis
-      @redis ||= Redis.new
-    end
+module CachedBitly
+  extend self
 
-    def redis=(redis)
-      @redis = redis
-    end
+  def redis
+    @redis ||= Redis.new
+  end
 
-    def redis_namespace
-      @redis_namespace ||= 'bitly'
-    end
+  def redis=(redis)
+    @redis = redis
+  end
 
-    def redis_namespace=(namespace)
-      @redis_namespace = namespace
-    end
+  def redis_namespace
+    @redis_namespace ||= 'bitly'
+  end
 
-    def allowed_hostnames
-      @allowed_hostnames ||= []
-    end
+  def redis_namespace=(namespace)
+    @redis_namespace = namespace
+  end
 
-    def allowed_hostnames=(hostnames=[])
-      @allowed_hostnames = hostnames
-    end
+  def allowed_hostnames
+    @allowed_hostnames ||= []
+  end
 
-    def bitly_client
-      @bitly_client ||= begin
-        Bitly.use_api_version_3
-        Bitly.new(ENV['BITLY_LOGIN'], ENV['BITLY_API_KEY'])
+  def allowed_hostnames=(hostnames=[])
+    @allowed_hostnames = hostnames
+  end
+
+  def bitly_client
+    @bitly_client ||= begin
+      Bitly.use_api_version_3
+      Bitly.new(ENV['BITLY_LOGIN'], ENV['BITLY_API_KEY'])
+    end
+  end
+
+  def bitly_client=(client)
+    @bitly_client = client
+  end
+
+  def stats_enabled?
+    !!@stats_enabled
+  end
+
+  def stats_enabled
+    @stats_enabled ||= false
+  end
+
+  def stats_enabled=(enabled)
+    @stats_enabled = enabled
+  end
+
+  def clean(html)
+    doc = html.is_a?(Nokogiri::HTML::Document) ? html : Nokogiri::HTML(html)
+    doc.css('a[href^=http]').each do |link|
+      url = link.attributes['href'].value
+      if !allowed_hostnames.empty? && !url.match(Regexp.union(*allowed_hostnames))
+        link.attributes['href'].value = fetch(url)
       end
     end
+    doc
+  end
 
-    def bitly_client=(client)
-      @bitly_client = client
+  # Handles retreiving cached short urls and generating new
+  # ones if we don't have the short url for a particular url.
+  #
+  # Returns short url, default if save goes wrong.
+  def fetch(url, default=url)
+    short_url = shortened url
+    if short_url
+      hit!
+      short_url
+    else
+      miss!
+      shorten(url) || default
     end
+  end
 
-    def clean(html)
-      doc = html.is_a?(Nokogiri::HTML::Document) ? html : Nokogiri::HTML(html)
-      doc.css('a[href^=http]').each do |link|
-        url = link.attributes['href'].value
-        if !self.class.allowed_hostnames.empty? && !url.match(Regexp.union(*self.class.allowed_hostnames))
-          link.attributes['href'].value = fetch(url, url)
-        end
-      end
-      doc
+  # Handles generating the short url and storing it.
+  #
+  # Returns short url if stored, false if not.
+  def shorten(url)
+    url = bitly_client.shorten(url)
+    if save(url.long_url, url.short_url)
+      url.short_url
+    else
+      false
     end
+  end
 
-    # Handles retreiving cached short urls and generating new
-    # ones if we don't have the short url for a particular url.
-    #
-    # Returns short url, default if save goes wrong.
-    def fetch(url, default=url)
-      short_url = shortened url
-      if short_url
-        hit!
-        short_url
-      else
-        miss!
-        shorten(url) || default
-      end
-    end
+  # Look to see if the url has already been shortened.
+  # If the url has been shortened, return the url.
+  #
+  # Returns short url if it has been shortened, nil if not
+  def shortened(url)
+    redis.hget("#{redis_namespace}:url", digest(url))
+  end
 
-    # Handles generating the short url and storing it.
-    #
-    # Returns short url if stored, false if not.
-    def shorten(url)
-      url = self.class.bitly_client.shorten(url)
-      if save(url.long_url, url.short_url)
-        url.short_url
-      else
-        false
-      end
-    end
+  # Save the url along with it's associated short url
+  # for easy retrieval.
+  #
+  # Returns true if saved, false if not.
+  def save(long_url, short_url)
+    !!redis.hset("#{redis_namespace}:url", digest(long_url), short_url)
+  end
 
-    # Look to see if the url has already been shortened.
-    # If the url has been shortened, return the url.
-    #
-    # Returns short url if it has been shortened, nil if not
-    def shortened(url)
-      redis.hget("#{redis_namespace}:url", digest(url))
-    end
+  def totals
+    { :hit   => redis.get("#{redis_namespace}:url:hit").to_i,
+      :miss  => redis.get("#{redis_namespace}:url:miss").to_i,
+      :total => redis.hlen("#{redis_namespace}:url") }
+  end
 
-    # Save the url along with it's associated short url
-    # for easy retrieval.
-    #
-    # Returns true if saved, false if not.
-    def save(long_url, short_url)
-      !!redis.hset("#{redis_namespace}:url", digest(long_url), short_url)
-    end
+  private
 
-    def totals
-      { :hit   => redis.get("#{redis_namespace}:url:hit").to_i,
-        :miss  => redis.get("#{redis_namespace}:url:miss").to_i,
-        :total => redis.hlen("#{redis_namespace}:url") }
-    end
+  def hit!
+    return unless stats_enabled?
+    redis.incr "#{redis_namespace}:url:hit"
+  end
 
-    private
+  def miss!
+    return unless stats_enabled?
+    redis.incr "#{redis_namespace}:url:miss"
+  end
 
-    def hit!
-      redis.incr "#{redis_namespace}:url:hit"
-    end
-
-    def miss!
-      redis.incr "#{redis_namespace}:url:miss"
-    end
-
-    def digest(object)
-      Digest::MD5.hexdigest object.to_s
-    end
+  def digest(object)
+    Digest::MD5.hexdigest object.to_s
   end
 end
